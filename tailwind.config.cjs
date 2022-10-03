2292d2ef6fa12b75d5a5df31d5d3e6645798c626
Abui <adjiem31@gmail.com>
2022-10-03 12:05:45 +0700

    feat: add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{html,js,astro}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
