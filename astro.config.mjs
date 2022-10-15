dff8b17db8fe6a2c3a08259ffaad10059c005908
Abui <adjiem31@gmail.com>
2022-10-15 23:13:05 +0700

    fix: robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import robotsTxt from 'astro-robots-txt';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -8,6 +9,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind(), react()],
-  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), tailwind(), react(), robotsTxt()],
+  site: 'https://portfolio-2k22.vercel.app/',
 });
