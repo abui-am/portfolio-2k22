d239f7087774d7fee0547bb554ff24739185f68f
Abui <adjiem31@gmail.com>
2022-10-16 23:06:24 +0700

    image

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import robotsTxt from 'astro-robots-txt';
+import image from '@astrojs/image';
 
 import tailwind from '@astrojs/tailwind';
 
@@ -9,6 +10,15 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), sitemap(), tailwind(), react(), robotsTxt()],
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    robotsTxt(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
   site: 'https://portfolio-2k22.vercel.app/',
 });
